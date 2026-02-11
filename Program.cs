30456355a1d95e520bdc9f5f28315a855fd66a52
Hiago 7k <hiago7k@gmail.com>
2026-02-11 19:34:10 -0300

    while

--- Program.cs
+++ Program.cs
@@ -28,7 +28,20 @@
 // }
 
 
-for (int i = 0; i <= 10; i++)
+// for (int i = 0; i <= 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+Boolean interruptor = false; // faz a magica
+
+
+while(interruptor)
 {
-    Console.WriteLine(i);
+    for(int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(i);
+    }
 }
+
