fd371e2b64b2b8e2b10971eb84ef1a423bb24a63
Hiago 7k <hiago7k@gmail.com>
2026-02-11 19:18:42 -0300

    revisao estudos apos ferias

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Conceitos Basicos
+// Variaveis -> utilizadas para armazenar dados na memoria do computador
+// Tipos -> definir um tipo da variavel number string boolean int decimal
+// Sintaxe ->  o jeito que escrevemos o codigo sempre respeitando a linguagem e utilizando ;
+// operadores -> < = > ! true or false
+// Condiceos -> definir se algo for verdadeiro executa se nao faz outra coisa if else
+// Funcoes blocos de codigos que chamamos para executar codigos, o c# ja vem com algumas pronta
+
+// 1 Solicitar ao usuario dizer o seu nome e sua idade e dizer ola nome do usuario voce tem idade do usuario
+
+Console.WriteLine("Ola qual o seu nome?");
+string nome = Console.ReadLine()!;
+
+Console.WriteLine($"Ola {nome}");
+
+Console.WriteLine("Qual sua idade?");
+int idade = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine($"{nome} voce tem {idade} anos!");
+
+if (idade >= 18)
+{
+    Console.WriteLine("Voce pode tirar sua CNH");
+}
+else
+{
+    Console.WriteLine("voce ainda e de menor");
+}
+
+
+
