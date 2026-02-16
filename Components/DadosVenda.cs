9c4871785d53dcb4950a9384f1e18c72d19f697a
Hiago 7k <hiago7k@gmail.com>
2026-02-16 14:29:09 -0300

    ajustando classe DadosVenda

--- Components/DadosVenda.cs
+++ Components/DadosVenda.cs
@@ -2,5 +2,27 @@ namespace revisar_conceitos_basicos.Components;
 
     public class DadosVenda
     {
-        public int MyProperty { get; set; }
+        
+        public string stemGuardOnOff;
+        public string skinsSelecionadas;
+        public string tradeURl;
+        public string telefone;
+
+    public DadosVenda(string stemGuardOnOff, string skinsSelecionadas, string tradeURl, string telefone)
+    {
+        this.stemGuardOnOff = stemGuardOnOff;
+        this.skinsSelecionadas = skinsSelecionadas;
+        this.tradeURl = tradeURl;
+        this.telefone = telefone;
     }
+
+
+    public void ExibirDadosDoUsuario()
+    {
+        Console.WriteLine(stemGuardOnOff);
+        Console.WriteLine(skinsSelecionadas);
+        Console.WriteLine(tradeURl);
+        Console.WriteLine(telefone);
+    }
+
+}
